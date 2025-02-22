e490fdd43d92567b418b59af9bb3ca6c212d0756
laura <contact0faztweb.com>
2025-02-22 13:04:44 +0100

    miw-frontend-foro #1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'frontend-foro' title`, () => {
+  it(`should have the 'miw-frontend-foro' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontend-foro');
+    expect(app.title).toEqual('miw-frontend-foro');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontend-foro');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, miw-frontend-foro');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend-foro';
+  title = 'miw-frontend-foro';
 }
